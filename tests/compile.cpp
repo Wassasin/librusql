8e9aef71642c8d9859ced020f04eae9de22a2228
Sjors Gielen <umc@sjorsgielen.nl>
2013-07-04 20:48:09 +0200

    Add 5 tests for rusql2. To run them, compile them and run them with options (host,user,pass,db) of an actually running MySQL server. Make sure the user has access to the (existing) db and it is empty. Once rusql gets embedded database support, you can also run them without parameters and they will test against the embedded database.

--- tests/compile.cpp
+++ tests/compile.cpp
@@ -1,6 +1,9 @@
 #include <rusql/rusql.hpp>
+#include "test.hpp"
+#include "database_test.hpp"
 
 int main() {
-	rusql::Database(rusql::Database::ConstructionInfo{"", "", "", ""});
+	test_init(1);
+	pass("Compiling against header works");
 	return 0;
 }
---
+++ tests/optional.cpp
@@ -0,0 +1,55 @@
+#include <rusql/rusql.hpp>
+#include <boost/optional.hpp>
+#include "test.hpp"
+#include "database_test.hpp"
+
+static bool contains(rusql::ResultSet &rs, std::string value) {
+	while(rs) {
+		if(rs.get_string(1) == value) {
+			return true;
+		}
+		rs.next();
+	}
+	return false;
+}
+
+int main(int argc, char *argv[]) {
+	test_init(4);
+	auto db = get_database(argc, argv);
+	db->execute("CREATE TABLE rusqltest (`value` VARCHAR(10) NULL)");
+
+	test_start_try(1);
+	try {
+		db->execute("INSERT INTO rusqltest VALUES (?)", boost::optional<std::string>("foo"));
+		auto res = db->query("SELECT * FROM rusqltest");
+		test(contains(res, "foo"), "could insert string from optional");
+	} catch(std::exception &e) {
+		diag(e);
+	}
+	test_finish_try();
+
+	db->execute("DELETE FROM rusqltest");
+	test_start_try(2);
+	try {
+		db->execute("INSERT INTO rusqltest VALUES (?)", boost::optional<std::string>());
+		auto res = db->query("SELECT * FROM rusqltest");
+		bool null = res.is_null(1);
+		pass("could ask if value was null");
+		test(null, "value inserted from empty optional was null");
+	} catch(std::exception &e) {
+		diag(e);
+	}
+	test_finish_try();
+
+	try {
+		auto res = db->query("SELECT * FROM rusqltest");
+		auto r = res.get_string(1);
+		diag("Result of get_string on null: '" + r + "'");
+		fail("get_string on null throws");
+	} catch(std::exception&) {
+		pass("get_string on null throws");
+	}
+
+	db->execute("DROP TABLE rusqltest");
+	return 0;
+}
