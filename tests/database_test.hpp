0b6ee55658dca935766ed31aaea17870cb0ce259
Sjors Gielen <umc@sjorsgielen.nl>
2013-07-05 20:38:42 +0200

    On invalid parameters to a test, give correct skip output and give it on stdout instead of stderr.

--- tests/database_test.hpp
+++ tests/database_test.hpp
@@ -2,14 +2,14 @@
 
 std::shared_ptr<rusql::Database> get_database(int argc, char *argv[]) {
 	if(argc == 1) {
-		std::cerr << "1..0 # Skipped: Embedded mode not supported yet" << std::endl;
+		std::cout << "1..0 # SKIP Embedded mode not supported yet" << std::endl;
 		exit(0);
 		//return new rusql::Database(rusql::Database::Embedded);
 	} else if(argc == 5) {
 		return std::make_shared<rusql::Database>(
 				rusql::Database::ConstructionInfo{argv[1], argv[2], argv[3], argv[4]});
 	} else {
-		std::cout << "1..0 # Skipped: Invalid parameters" << std::endl;
+		std::cout << "1..0 # SKIP Invalid parameters" << std::endl;
 		exit(0);
 	}
 }
