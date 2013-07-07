2cec1d30e1a37885753327560dcac92551ad869e
Sjors Gielen <umc@sjorsgielen.nl>
2013-07-07 03:52:24 +0200

    Make the query test more robust by enclosing all tests in try-catch blocks, and add some INSERT and more SELECT queries.

--- tests/query.cpp
+++ tests/query.cpp
@@ -15,12 +15,59 @@ static bool contains(rusql::ResultSet &rs, std::string value) {
 
 int main(int argc, char *argv[]) {
 	auto db = get_database(argc, argv);
-	test_init(2);
-	db->execute("CREATE TABLE rusqltest (`value` INT(2) NOT NULL)");
-	auto res = db->query("SHOW TABLES");
-	test(contains(res, "rusqltest"), "rusqltest table created");
-	db->execute("DROP TABLE rusqltest");
-	res = db->query("SHOW TABLES");
-	test(!contains(res, "rusqltest"), "rusqltest table dropped");
+	test_init(8);
+	try {
+		db->execute("CREATE TABLE rusqltest (`value` INT(2) NOT NULL)");
+		pass("could run CREATE TABLE");
+	} catch(std::exception &e) {
+		diag(e);
+		fail("could run CREATE TABLE");
+	}
+	test_start_try(1);
+	try {
+		auto res = db->query("SHOW TABLES");
+		test(contains(res, "rusqltest"), "rusqltest table created");
+	} catch(std::exception &e) {
+		diag(e);
+	}
+	test_finish_try();
+
+	test_start_try(1);
+	try {
+		// SELECT on empty database
+		auto res = db->query("SELECT * FROM rusqltest");
+		test(!res, "no result rows in empty table");
+	} catch(std::exception &e) {
+		diag(e);
+	}
+	test_finish_try();
+
+	test_start_try(3);
+	try {
+		db->execute("INSERT INTO rusqltest (`value`) VALUES (27)");
+		auto res = db->query("SELECT * FROM rusqltest");
+		test(res, "result row in empty table");
+		test(res.get_uint64(0) == 27, "correct result row in empty table");
+		test(res.get_uint64("value") == 27, "correct result row by name in empty table");
+	} catch(std::exception &e) {
+		diag(e);
+	}
+	test_finish_try();
+
+	try {
+		db->execute("DROP TABLE rusqltest");
+		pass("could run DROP TABLE");
+	} catch(std::exception &e) {
+		diag(e);
+		fail("could run DROP TABLE");
+	}
+	test_start_try(1);
+	try {
+		auto res = db->query("SHOW TABLES");
+		test(!contains(res, "rusqltest"), "rusqltest table dropped");
+	} catch(std::exception &e) {
+		diag(e);
+	}
+	test_finish_try();
 	return 0;
 }
