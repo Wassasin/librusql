b2110a0f33e17e45669cc625ffb94cdd6da411ee
Sjors Gielen <umc@sjorsgielen.nl>
2013-07-17 18:02:05 +0200

    Add test for last commit: test if optional<int> works.

--- tests/optional.cpp
+++ tests/optional.cpp
@@ -3,9 +3,10 @@
 #include "test.hpp"
 #include "database_test.hpp"
 
-static bool contains(rusql::ResultSet &rs, std::string value) {
+template <typename T>
+static bool contains(rusql::ResultSet &rs, const T &value) {
 	while(rs) {
-		if(rs.get_string(0) == value) {
+		if(rs.get<T>(size_t(0)) == value) {
 			return true;
 		}
 		rs.next();
@@ -15,14 +16,14 @@ static bool contains(rusql::ResultSet &rs, std::string value) {
 
 int main(int argc, char *argv[]) {
 	auto db = get_database(argc, argv);
-	test_init(11);
+	test_init(18);
 	db->execute("CREATE TABLE rusqltest (`value` VARCHAR(10) NULL)");
 
 	test_start_try(1);
 	try {
 		db->execute("INSERT INTO rusqltest VALUES (?)", boost::optional<std::string>("foo"));
 		auto res = db->query("SELECT * FROM rusqltest");
-		test(contains(res, "foo"), "could insert string from optional");
+		test(contains(res, std::string("foo")), "could insert string from optional");
 	} catch(std::exception &e) {
 		diag(e);
 	}
@@ -57,12 +58,11 @@ int main(int argc, char *argv[]) {
 		auto statement = db->execute("SELECT * FROM rusqltest");
 		statement.store_result();
 
-		boost::optional<std::string> value;
+		boost::optional<std::string> value("some initialized value");
 		statement.bind_results(value);
 		test(statement.num_rows() == 2, "two results in num_rows()");
 		test(statement.fetch(), "first result");
 		test(!value, "first result is null");
-		value = std::string("enough space for the next value");
 		test(statement.fetch(), "second results");
 		test(value, "second result is set");
 		test(*value == "a", "second result is set correctly");
@@ -73,5 +73,28 @@ int main(int argc, char *argv[]) {
 	test_finish_try();
 
 	db->execute("DROP TABLE rusqltest");
+	db->execute("CREATE TABLE rusqltest (value INT(10) NULL)");
+
+	test_start_try(7);
+	try {
+		db->execute("INSERT INTO rusqltest VALUES (?)", boost::optional<int>());
+		db->execute("INSERT INTO rusqltest VALUES (?)", boost::optional<int>(5));
+		auto statement = db->execute("SELECT * FROM rusqltest");
+		statement.store_result();
+
+		boost::optional<int> value = 0xdeadbeef;
+		statement.bind_results(value);
+		test(statement.num_rows() == 2, "two results in num_rows after inserting optional ints");
+		test(statement.fetch(), "first result");
+		test(!value, "first result is null");
+		test(statement.fetch(), "second result");
+		test(value, "second result is set");
+		test(*value == 5, "second result is set correctly");
+		test(!statement.fetch(), "end of results");
+	} catch(std::exception &e) {
+		diag(e);
+	}
+	test_finish_try();
+
 	return 0;
 }
