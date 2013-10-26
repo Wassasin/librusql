c5074e635a1eacee6b3c30cc8b5f332bb1098b25
Sjors Gielen <umc@sjorsgielen.nl>
2013-10-26 15:54:39 +0200

    Revert "Find MacPort's MySQL". The mysql.pc it used was not shipped by upstream, so cannot be supported everywhere. Upstream, a binary called 'mysql_config' is shipped which is supposed to return the right flags for every OS, also MacPorts.
    This reverts commit 0e2a084070068cfeb44e871680a4a26685699c35.

--- cmake/modules/FindMYSQL.cmake
+++ cmake/modules/FindMYSQL.cmake
@@ -1,36 +1,90 @@
 # - Find MySQL
+# Find the MySQL includes and client library
+# This module defines
+#  MYSQL_INCLUDE_DIR, where to find mysql.h
+#  MYSQL_LIBRARIES, the libraries needed to use MySQL.
+#  MYSQL_FOUND, If false, do not try to use MySQL.
+#  MYSQLd_LIBRARIES, the libraries needed to use MySQL Embedded.
+#  MYSQLd_FOUND, If false, do not try to use MySQL Embedded.
 #
-# MYSQL_FOUND
-# MYSQL_INCLUDE_DIRS
-# MYSQL_LIBRARIES
+# Copyright (c) 2006, Jaroslaw Staniek, <js@iidea.pl>
+# Updated by Sjors Gielen <mysql@sjor.sg>
+# * Support for MySQL Embedded added
+# * Hard-coded support for Windows removed
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-include(LibFindMacros)
+SET(MYSQL_CONFIG_PREFER_PATH "$ENV{MYSQL_HOME}/bin" CACHE FILEPATH
+  "preferred path to MySQL (mysql_config)")
 
-libfind_package(MYSQL zlib)
-
-libfind_pkg_check_modules(MYSQL_PKGCONF mysql)
-
-find_path(MYSQL_INCLUDE_DIR
-	NAMES mysql.h
-	PATHS
-	${MYSQL_PKGCONF_INCLUDE_DIRS}
-	/usr/local/include/mysql/
-	/usr/include/mysql/
-	/sw/include/mysql/
-	/opt/local/include/mysql55/mysql
+FIND_PROGRAM(MYSQL_CONFIG mysql_config
+  ${MYSQL_CONFIG_PREFER_PATH}
+  /usr/local/mysql/bin/
+  /usr/local/bin/
+  /usr/bin/
 )
 
-find_library(MYSQL_LIBRARY
-	NAMES mysql mysqlclient
-	PATHS ${MYSQL_PKGCONF_LIBRARY_DIRS}
-	/usr/local/lib/mysql/
-	/usr/lib/mysql/
-	/sw/lib/mysql/
-	/opt/local/lib/mysql55/mysql/
-)
+IF (MYSQL_CONFIG)
+	EXEC_PROGRAM(${MYSQL_CONFIG}
+		ARGS --include
+		OUTPUT_VARIABLE MY_TMP)
+	string (REGEX REPLACE "-I([^ ]*)( .*)?" "\\1" MY_TMP "${MY_TMP}")
+	SET(MYSQL_INCLUDE_DIR ${MY_TMP} CACHE FILEPATH INTERNAL)
 
-# Set the include dir variables and the libraries and let libfind_process do the rest.
-# NOTE: Singular variables for this library, plural for libraries this this lib depends on.
-set(MYSQL_PROCESS_INCLUDES MYSQL_INCLUDE_DIR ZLIB_INCLUDE_DIRS)
-set(MYSQL_PROCESS_LIBS MYSQL_LIBRARY ZLIB_LIBRARIES)
-libfind_process(MYSQL)
+	EXEC_PROGRAM(${MYSQL_CONFIG}
+		ARGS --libs
+		OUTPUT_VARIABLE MY_TMP)
+
+	string(REGEX MATCHALL "(^| )-l[^ ]*" MYSQL_LIB_LIST "${MY_TMP}")
+	foreach(LIB ${MYSQL_LIB_LIST})
+		string(REGEX REPLACE "[ ]*-l([^ ]*)" "\\1" LIB "${LIB}")
+		list(APPEND MYSQL_LIBRARIES "${LIB}")
+		#message("[DEBUG] MYSQL ADD_LIBRARIES : ${MYSQL_LIBRARIES}")
+	endforeach(LIB ${MYSQL_LIB_LIST})
+
+	set(MYSQL_ADD_LIBRARIES_PATH "")
+	string(REGEX MATCHALL "(^| )-L[^ ]*" MYSQL_LIBDIR_LIST "${MY_TMP}")
+	foreach(LIB ${MYSQL_LIBDIR_LIST})
+		string(REGEX REPLACE "[ ]*-L([^ ]*)" "\\1" LIB "${LIB}")
+		list(APPEND MYSQL_LIBRARIES_PATH "${LIB}")
+		#message("[DEBUG] MYSQL ADD_LIBRARIES_PATH : ${MYSQL_LIBRARIES_PATH}")
+	endforeach(LIB ${MYSQL_LIBDIR_LIST})
+
+	EXEC_PROGRAM(${MYSQL_CONFIG}
+		ARGS --libmysqld-libs
+		OUTPUT_VARIABLE MY_TMP)
+
+	string(REGEX MATCHALL "(^| )-l[^ ]*" MYSQL_LIB_LIST "${MY_TMP}")
+	foreach(LIB ${MYSQL_LIB_LIST})
+		string(REGEX REPLACE "[ ]*-l([^ ]*)" "\\1" LIB "${LIB}")
+		list(APPEND MYSQLd_LIBRARIES "${LIB}")
+		#message("[DEBUG] MYSQLd ADD_LIBRARIES : ${MYSQLd_LIBRARIES}")
+	endforeach(LIB ${MYSQL_LIB_LIST})
+
+	set(MYSQLd_LIBRARIES_PATH "")
+	string(REGEX MATCHALL "(^| )-L[^ ]*" MYSQL_LIBDIR_LIST "${MY_TMP}")
+	foreach(LIB ${MYSQL_LIBDIR_LIST})
+		string(REGEX REPLACE "[ ]*-L([^ ]*)" "\\1" LIB "${LIB}")
+		list(APPEND MYSQLd_LIBRARIES_PATH "${LIB}")
+		#message("[DEBUG] MYSQLd ADD_LIBRARIES_PATH : ${MYSQLd_LIBRARIES_PATH}")
+	endforeach(LIB ${MYSQL_LIBDIR_LIST})
+ENDIF(MYSQL_CONFIG)
+
+if(MYSQL_INCLUDE_DIR AND MYSQL_LIBRARIES)
+   set(MYSQL_FOUND TRUE CACHE INTERNAL "MySQL found")
+   message(STATUS "Found MySQL: ${MYSQL_INCLUDE_DIR}, ${MYSQL_LIBRARIES}")
+else(MYSQL_INCLUDE_DIR AND MYSQL_LIBRARIES)
+   set(MYSQL_FOUND FALSE CACHE INTERNAL "MySQL found")
+   message(STATUS "MySQL not found.")
+endif(MYSQL_INCLUDE_DIR AND MYSQL_LIBRARIES)
+
+if(MYSQL_INCLUDE_DIR AND MYSQLd_LIBRARIES)
+   set(MYSQLd_FOUND TRUE CACHE INTERNAL "MySQL Embedded found")
+   message(STATUS "Found MySQLd: ${MYSQL_INCLUDE_DIR}, ${MYSQLd_LIBRARIES}")
+else(MYSQL_INCLUDE_DIR AND MYSQLd_LIBRARIES)
+   set(MYSQLd_FOUND FALSE CACHE INTERNAL "MySQL Embedded found")
+   message(STATUS "MySQLd not found.")
+endif(MYSQL_INCLUDE_DIR AND MYSQLd_LIBRARIES)
+
+mark_as_advanced(MYSQL_INCLUDE_DIR MYSQL_LIBRARIES MYSQLd_LIBRARIES)
