a402cc4790a9031f3c205d565ecc2f10a742ae60
Sjors Gielen <umc@sjorsgielen.nl>
2013-11-04 04:19:56 +0100

    Add some commented out code for easier debugging later

--- rusql/token.hpp
+++ rusql/token.hpp
@@ -2,5 +2,8 @@
 
 namespace rusql {
 	//! Is shared between a Connection and a (Statement or ResultSet), so a connection knows when it's still in use
-	struct Token{};
+	struct Token{
+	// Token() { std::cout << "Token CREATED" << std::endl; }
+	// ~Token() { std::cout << "Token DESTROYED" << std::endl; }
+	};
 }
