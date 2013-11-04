75e0fcfe4e51aec2ee410ff5a9dfc49106bd1183
Sjors Gielen <umc@sjorsgielen.nl>
2013-11-04 04:24:59 +0100

    Add level of indentation

--- tests/threads.cpp
+++ tests/threads.cpp
@@ -9,27 +9,29 @@ int main(int argc, char *argv[]) {
 	db->execute("CREATE TABLE rusqltest (`value` INT(2) NOT NULL)");
 	db->execute("INSERT INTO rusqltest VALUES (20)");
 
-	auto statement = db->prepare("SELECT value FROM rusqltest");
+	{
+		auto statement = db->prepare("SELECT value FROM rusqltest");
 
-	statement.execute();
+		statement.execute();
 
-	boost::thread thread([&db]() {
-		auto thread_handle = db->get_thread_handle();
-		db->execute("UPDATE rusqltest SET value=30");
-	});
-	thread.join();
+		boost::thread thread([&db]() {
+			auto thread_handle = db->get_thread_handle();
+			db->execute("UPDATE rusqltest SET value=30");
+		});
+		thread.join();
 
-	uint64_t value = 10;
-	statement.bind_results(value);
-	test(statement.fetch(), "one result");
-	test(value == 20, "value was correct (" + std::to_string(value) + ")");
-	test(!statement.fetch(), "exactly one result");
+		uint64_t value = 10;
+		statement.bind_results(value);
+		test(statement.fetch(), "one result");
+		test(value == 20, "value was correct (" + std::to_string(value) + ")");
+		test(!statement.fetch(), "exactly one result");
 
-	statement.execute();
-	statement.bind_results(value);
-	test(statement.fetch(), "one result");
-	test(value == 30, "value was correct (" + std::to_string(value) + ")");
-	test(!statement.fetch(), "exactly one result");
+		statement.execute();
+		statement.bind_results(value);
+		test(statement.fetch(), "one result");
+		test(value == 30, "value was correct (" + std::to_string(value) + ")");
+		test(!statement.fetch(), "exactly one result");
+	}
 
 	db->execute("DROP TABLE rusqltest");
 }
